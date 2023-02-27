1ac831d690925b326e302b811b5fd29ca773dd57
flvsglezcano <109612063+flvsglezcano@users.noreply.github.com>
2023-02-27 13:56:38 -0500

    Init vue project setup

---
+++ studentProfilePagesUI/src/components/CreateSpp.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="greetings">
+    <h1 class="green">Create your Profile</h1>
+    <form>
+      <div>
+        <h3>
+          <span id="firstName">First Name: </span>
+          <input>
+          <br />
+
+          <span id="lastName">Last Name: </span>
+          <input>
+          <br />
+
+          <span id="grade">Grade: </span>
+          <input>
+          <br />
+
+          <span id="dob">Date of Birth: </span>
+          <input type="date">
+          <br />
+
+          <span>Phone Number: </span>
+          <input type="tel" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}">
+          <br />
+
+          <span>Phone Type: </span>
+          <select>
+            <option value="mobile">Mobile</option>
+            <option value="home">Home</option>
+          </select>
+          <br />
+
+          <span>Address Line 1: </span>
+          <input>
+          <br />
+
+          <span>Address Line 2: </span>
+          <input>
+          <br />
+
+          <span>City: </span>
+          <input>
+          <br />
+
+          <span>State: </span>
+          <input>
+          <br />
+
+          <span>Zip Code: </span>
+          <input>
+          <br />
+
+          <span>Country: </span>
+          <input>
+          <br />
+
+          <input type="submit" value="Submit">
+
+        </h3>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ studentProfilePagesUI/src/components/ViewSpp.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="greetings">
+    <h1 class="green">View your profile</h1>
+    <div>
+      <h3>
+        <span id="firstName">First Name: </span>
+        <span>{{ studentProfileDataSet.firstName }}</span>
+        <br/>
+
+        <span id="lastName">Last Name: </span>
+        <span>{{ studentProfileDataSet.lastName }}</span>
+        <br/>
+
+        <span id="grade">Grade: </span>
+        <span>{{ studentProfileDataSet.grade }}</span>
+        <br/>
+
+        <span id="dob">Date of Birth: </span>
+        <span>{{ studentProfileDataSet.dateOfBirth }}</span>
+        <br/>
+
+        <span>Phone Number: </span>
+        <span>{{ studentProfileDataSet.phoneNumber }}</span>
+        <br/>
+
+        <span>Phone Type: </span>
+        <span>{{ studentProfileDataSet.phoneType }}</span>
+        <br/>
+
+        <span>Address Line 1: </span>
+        <span>{{ studentProfileDataSet.addressLine1 }}</span>
+        <br/>
+        
+        <span>Address Line 2: </span>
+        <span>{{ studentProfileDataSet.addressLine2 }}</span>
+        <br/>
+        
+        <span>City: </span>
+        <span>{{ studentProfileDataSet.city }}</span>
+        <br/>
+        
+        <span>State: </span>
+        <span>{{ studentProfileDataSet.state }}</span>
+        <br/>
+        
+        <span>Zip Code: </span>
+        <span>{{ studentProfileDataSet.zipCode }}</span>
+        <br/>
+        
+        <span>Country: </span>
+        <span>{{ studentProfileDataSet.country }}</span>
+        <br/>
+        
+      </h3>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  studentProfileDataSet: {
+
+  }
+})
+</script>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
